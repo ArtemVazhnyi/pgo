0726166a9ef5b92e574198905cf6d2ac2b5c4b22
s33007 <s33007@pjwstk.edu.pl>
2025-03-13 09:11:04 +0100

    Ćwiczenie

--- javaCode/src/Main.java
+++ javaCode/src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello world!");
-            int number = 10;
-            System.out.println(number);
+
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arrTable = filterEvenIndexOddValue(arr);
     }
+
+    public static int[] filterEvenIndexOddValue(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                if (array[i] % 2 != 0) {
+                    System.out.print(array[i] + " ");
+                }
+            }
+        }
+        return array;
+    }
+
 }
