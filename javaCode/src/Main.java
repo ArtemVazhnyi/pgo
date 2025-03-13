a425bbb1194176f176f1ea5cf021679886a02ab9
s33007 <s33007@pjwstk.edu.pl>
2025-03-13 09:38:25 +0100

    Ćwiczenie

--- javaCode/src/Main.java
+++ javaCode/src/Main.java
@@ -1,19 +1,44 @@
 public class Main {
-    public static void main(String[] args) {
+    public static int[] filterEvenIndexOddValue(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0 && arr[i] % 2 != 0) {
+                count++;
+            }
+        }
 
-        int[] arr = {1, 2, 3, 4, 5, 6};
-        int[] arrTable = filterEvenIndexOddValue(arr);
+        int[] result = new int[count];
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0 && arr[i] % 2 != 0) {
+                result[index++] = arr[i];
+            }
+        }
+
+        return result;
     }
 
-    public static int[] filterEvenIndexOddValue(int[] array) {
+    public static int findDominant(int[] array) {
+        int count = 0;
         for (int i = 0; i < array.length; i++) {
-            if (i % 2 == 0) {
-                if (array[i] % 2 != 0) {
-                    System.out.print(array[i] + " ");
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    count++;
                 }
             }
         }
-        return array;
+        return count;
     }
 
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr2 = {3, 3, 3, 2, 2};
+        int[] filtered = filterEvenIndexOddValue(arr);
+        int[] filtered2 = filterEvenIndexOddValue(arr2);
+
+        for (int num : filtered) {
+            System.out.print(num + " ");
+        }
+    }
 }
---
+++ javaCode/src/Main2.java
@@ -0,0 +1,26 @@
+public class Main2 {
+
+    public static int findDominant(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            int candidate = nums[i];
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == candidate) {
+                    count++;
+                }
+            }
+
+            if (count > nums.length / 2) {
+                return candidate;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 3, 3, 2, 2};
+        System.out.println(findDominant(nums));
+    }
+}
